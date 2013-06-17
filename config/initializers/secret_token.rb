a54441dbb458ecdf609c94d389d6416e95ddb93e
Carter Cowan <hornsfan97@gmail.com>
2013-06-17 13:49:33 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrimeApp::Application.config.secret_token = 'af297feaf2e3397a8c047b81add730fc7b0560dd7944998da060bdf012ef3296b906fbf20052db4155f0d4af55814512156eb0ea40d3297d402ccd2637432cf1'
